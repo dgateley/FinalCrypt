a48a2bb1cdbe30f4b2e4b9e127ce6392aaf39244
dlgateley@cst.local <dlgateley@cst.local>
2017-08-11 09:55:56 -0500

    Implemented WindowMaximizeButton

--- FinalCrypt/src/Controls/FinalCryptWindowMaximizeButton.cs
+++ FinalCrypt/src/Controls/FinalCryptWindowMaximizeButton.cs
@@ -10,7 +10,47 @@ using FinalCrypt.Theme;
 namespace FinalCrypt.Controls
 {
     class FinalCryptWindowMaximizeButton : Button
-    {
-       
+    { 
+        public bool Clickable { get; set; } = true;
+
+
+        private SolidBrush brush = new SolidBrush(Color.FromArgb(255, 10, 10, 10));
+        private SolidBrush hoveredBrush = new SolidBrush(Color.FromArgb(255, 20, 20, 20));
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle rect = new Rectangle(6, 6, 10, 10);
+
+
+            Pen pen = new Pen(Color.White, 2);
+
+            if (!Clickable)
+            {
+                brush = new SolidBrush(Color.FromArgb(255, 20, 20, 20));
+                hoveredBrush = new SolidBrush(Color.FromArgb(255, 50, 50, 50));
+
+                if (ClientRectangle.Contains(PointToClient(Cursor.Position)))
+                    e.Graphics.FillRectangle(hoveredBrush, e.ClipRectangle);
+                else
+                    e.Graphics.FillRectangle(brush, e.ClipRectangle);
+            }
+            else
+            {
+                if (ClientRectangle.Contains(PointToClient(Cursor.Position)))
+                    e.Graphics.FillRectangle(hoveredBrush, e.ClipRectangle);
+                else
+                    e.Graphics.FillRectangle(brush, e.ClipRectangle);
+            }
+
+            e.Graphics.DrawRectangle(pen, rect);
+
+            pen.Dispose();
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            if (Clickable)
+                FindForm().WindowState = FormWindowState.Maximized;
+        }
     }
 }
