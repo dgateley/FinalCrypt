34afc48eb122434c71a6fd3d814026fdb499f0ec
_decaf <hjeffens@yahoo.com>
2017-08-12 15:54:14 -0500

    Added Register form

---
+++ FinalCrypt/src/Forms/frmRegister.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FinalCrypt.Forms
+{
+    partial class frmRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRegister = new FinalCrypt.Controls.FinalCryptButton();
+            this.lblUsername = new FinalCrypt.Controls.FinalCryptLabel();
+            this.lblPassword = new FinalCrypt.Controls.FinalCryptLabel();
+            this.txtPassword = new FinalCrypt.Controls.FinalCryptTextBox();
+            this.txtUsername = new FinalCrypt.Controls.FinalCryptTextBox();
+            this.SuspendLayout();
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegister.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.btnRegister.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(5)))), ((int)(((byte)(5)))));
+            this.btnRegister.Location = new System.Drawing.Point(338, 149);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(121, 35);
+            this.btnRegister.TabIndex = 10;
+            this.btnRegister.Text = "Create Account";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.lblUsername.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.lblUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(5)))), ((int)(((byte)(5)))));
+            this.lblUsername.Location = new System.Drawing.Point(42, 52);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(75, 20);
+            this.lblUsername.TabIndex = 6;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.lblPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(5)))), ((int)(((byte)(5)))));
+            this.lblPassword.Location = new System.Drawing.Point(42, 101);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(70, 20);
+            this.lblPassword.TabIndex = 7;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.txtPassword.Location = new System.Drawing.Point(127, 99);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Padding = new System.Windows.Forms.Padding(1);
+            this.txtPassword.PasswordHidden = false;
+            this.txtPassword.Size = new System.Drawing.Size(260, 28);
+            this.txtPassword.TabIndex = 9;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.txtUsername.Location = new System.Drawing.Point(127, 50);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Padding = new System.Windows.Forms.Padding(1);
+            this.txtUsername.PasswordHidden = false;
+            this.txtUsername.Size = new System.Drawing.Size(296, 28);
+            this.txtUsername.TabIndex = 8;
+            // 
+            // frmRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 196);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Name = "frmRegister";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FinalCrypt - Register";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmRegister_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmRegister_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controls.FinalCryptButton btnRegister;
+        private Controls.FinalCryptLabel lblUsername;
+        private Controls.FinalCryptLabel lblPassword;
+        private Controls.FinalCryptTextBox txtPassword;
+        private Controls.FinalCryptTextBox txtUsername;
+    }
+}
---
+++ FinalCrypt/src/Forms/frmRegister.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FinalCrypt.Theme;
+using FinalCrypt.DB;
+
+namespace FinalCrypt.Forms
+{
+    public partial class frmRegister : Form
+    {
+        private bool passwordVisible;
+
+        public bool PasswordVisible
+        {
+            get { return passwordVisible; }
+
+            set
+            {
+                passwordVisible = value;
+                txtPassword.textBox.UseSystemPasswordChar = value;
+            }
+        }
+
+        public frmRegister()
+        {
+            InitializeComponent();
+
+            ThemeWindowCommands.WindowSettings windowSettings = new ThemeWindowCommands.WindowSettings();
+            windowSettings.Maximizable = false;
+            windowSettings.CloseType = ThemeWindowCommands.CloseTypes.Close;
+            ThemeStyling.Style(this, windowSettings);
+
+            PasswordVisible = false;
+
+            txtPassword.AddButton();
+        }
+
+        // Handle drawing
+        private void frmRegister_Paint(object sender, PaintEventArgs e)
+        {
+            ThemeDrawing.DrawWindowHandle(this);
+        }
+
+        // Handle dragging
+        private void frmRegister_MouseDown(object sender, MouseEventArgs e)
+        {
+            ThemeInteraction.Drag(this, e);
+        }
+
+        // Register the user
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            // Validate input
+            if (txtUsername.textBox.Text == "" || txtPassword.textBox.Text == "")
+            {
+                MessageBox.Show("Please fill out both fields", "Invalid Registration");
+                return;
+            }
+
+            bool success = DBOperations.AttemptRegister(txtUsername.textBox.Text, txtPassword.textBox.Text);
+
+            if (success)
+            {
+                MessageBox.Show("User account has been registered. You may now login.");
+                Close();
+            }
+        }
+    }
+}
